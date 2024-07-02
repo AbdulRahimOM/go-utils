a6fa446dceb10e60b09b0ec37767387d3c4be80e
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-07-02 10:28:47 +0530

    added function: string to int32 convert

--- math/my_math.go
+++ math/my_math.go
@@ -2,9 +2,19 @@ package myMath
 
 import (
 	"math"
+	"strconv"
 )
 
 func RoundFloat32(value float32, decimalPlaces float64) float32 {
 	divisor := math.Pow(10, decimalPlaces)
 	return float32(math.Round(float64(value)*divisor) / divisor)
+}
+
+// StringToInt32 converts a string to an int32.
+func StringToInt32(s string) (int32, error) {
+	i, err := strconv.ParseInt(s, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return int32(i), nil
 }
