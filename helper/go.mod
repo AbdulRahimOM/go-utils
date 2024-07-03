9120e0329f0fd3fd0d289cd30a3e941035a14e39
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-07-03 07:55:35 +0530

    helper package

---
+++ helper/go.mod
@@ -0,0 +1,3 @@
+module github.com/AbdulRahimOM/go-utils/helper
+
+go 1.22.2
---
+++ helper/random.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"time"
+	"math/rand"
+
+)
+
+// Helper function to generate a random alphanumeric string
+func GenerateRandomAlphanumeric(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
