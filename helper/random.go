3e08a5bcd862868a68db4a2be808b2e3725d5d7d
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-07-10 19:44:30 +0530

    helper->random numeric function

--- helper/random.go
+++ helper/random.go
@@ -16,3 +16,14 @@ func GenerateRandomAlphanumeric(length int) string {
 	}
 	return string(b)
 }
+
+// Helper function to generate a random alphanumeric string
+func GenerateRandomNumeric(length int) string {
+	const charset = "0123456789"
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
